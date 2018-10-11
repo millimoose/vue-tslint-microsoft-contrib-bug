2e28733fa0e55a95b04f96e59b3bfe087a1ea4f0
David Vallner <david@vallner.net>
2018-10-11 14:42:30 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<script lang="ts">
+//tslint:disable missing-jsdoc
+import Vue from 'vue';
+export default class App extends Vue {}
+</script>
---
+++ src/shims-vue.d.ts
@@ -0,0 +1,5 @@
+//tslint:disable missing-jsdoc
+declare module '*.vue' {
+  import Vue from 'vue';
+  export default Vue;
+}
